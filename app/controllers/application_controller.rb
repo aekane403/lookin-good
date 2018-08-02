9183b24530f3b7be9289f57ba5bfa0f8a82b50ac
Abigail Kane <aekane403@gmail.com>
2018-08-02 21:05:22 +0000

    working version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,35 @@ class ApplicationController < Sinatra::Base
     erb :temp
   end
   
+  get "/template" do
+    erb :template
+  end
+  
   post "/results" do
-    params.to_s
+    bn = params["price"] + params["style"]
+    brands = {"1t"=>"Forever21","1c"=>"old_navy","1p"=>"HM","2t"=>"Zara","2c"=>"Nike_Adidas","2t"=>"J_Crew","3t"=>"Urban_outfitters","3c"=>"lulu","3p"=>"Ralph_Lauren"}
+    if bn == "1t"
+      erb :forever_21
+    elsif bn == "1c"
+      erb :gap
+    elsif bn == "1p"
+      erb :hm
+    elsif bn == "2t"
+      erb :zara
+    elsif bn == "2c"
+      erb :nike
+    elsif bn == "2p"
+      erb :crew
+    elsif bn == "3t"
+      erb :urban
+    elsif bn =="3c"
+      erb :lulu
+    elsif bn == "3p"
+      erb :ralph
+    else 
+      "Sorry We Could Not Find A Brand For You"
+    end
   end
   
 end
+
